6fb48a83a5befe99476c124f98b33119c6e1e362
PT Investasi Ecoenergi Indonesia <invesco.ofc@gmail.com>
2025-08-14 04:43:46 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        // TAMBAHKAN BARIS INI
-      base: '/invesco-id.github.io/',
-        
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
